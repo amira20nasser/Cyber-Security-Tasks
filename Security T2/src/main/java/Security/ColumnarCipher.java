ab5dac24ae488692b7f63aebf356b93a7f064a88
amira_nasser <amira20nasser@gmail.com>
2025-04-14 00:28:05 +0200

    ✨ feat(Columnar Algorithm): implemented analyse function

--- Security T2/src/main/java/Security/ColumnarCipher.java
+++ Security T2/src/main/java/Security/ColumnarCipher.java
@@ -5,8 +5,54 @@ public class ColumnarCipher {
 
     public List<Integer> analyse(String plainText, String cipherText) {
         // TODO: Analyze the plainText and cipherText to determine the key(s)
-
-        return new ArrayList<>(); // Placeholder return
+        int keyLen = getKeyLen( plainText,  cipherText);
+        System.out.println(keyLen);
+        int rows = plainText.length() / keyLen;
+        List<Integer> keys = new ArrayList<>(); 
+        char [] firstRowColumnar = new char[keyLen]; // C O M P U
+        char [] cipher = new char[keyLen]; // C P O M U
+        int index = 0;
+        for(int i =0; i<keyLen;i++){
+            cipher[i] = cipherText.charAt(index);
+            index+=rows;
+        }
+        for(int i =0; i<keyLen;i++){
+            firstRowColumnar[i] = plainText.charAt(i);
+        }
+       for(int j =0; j<keyLen;j++){
+        for(int l=0;l<keyLen;l++){
+            if(firstRowColumnar[j]== cipher[l]){
+                keys.add(l+1);
+                break;
+            }
+        }
+       } 
+        // for(int j =0; j<cipherText.length();){
+        //     Character c = cipherText.charAt(j);
+        //     for (int k=0;k<keyLen;k++){
+        //         if(c==firstRowColumnar[k]){
+        //             keys.add(k+1);
+        //             break;
+        //         }
+        //     }
+        //     j = j + rows;
+        // }
+        return  keys; // Placeholder return
+    }
+    private int getKeyLen(String plainText, String cipherText){
+        Character c1 = cipherText.charAt(0);
+        Character c2 = cipherText.charAt(1); 
+        for (int i = 0 ; i < plainText.length() ; i++){
+            if (plainText.charAt(i) == c1){
+                for (int j = i+1 ; j < plainText.length() ; j++){
+                    if(c2 == plainText.charAt(j)){
+                        int keyLen = j;
+                        return keyLen;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public String decrypt(String cipherText, List<Integer> key) {
