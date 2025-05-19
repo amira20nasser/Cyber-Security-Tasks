00055941f57bb1e646c381e9ae276806cefa7bca
amira_nasser <amira20nasser@gmail.com>
2025-05-19 22:38:34 +0300

    feat(RSA): added encryption

--- Security T3/src/main/java/Security/RSA.java
+++ Security T3/src/main/java/Security/RSA.java
@@ -1,9 +1,22 @@
 package Security;
 
+import java.math.BigInteger;
+
 public class RSA {
 
     public int encrypt(int p, int q, int M, int e) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        
+        BigInteger pp = BigInteger.valueOf(p);
+        BigInteger qq = BigInteger.valueOf(q);
+        BigInteger MM = BigInteger.valueOf(M);
+        BigInteger ee = BigInteger.valueOf(e);
+
+        BigInteger n = pp.multiply(qq);
+
+        BigInteger cipher = MM.modPow(ee, n);
+
+        return cipher.intValue();
+    
     }
 
     public int decrypt(int p, int q, int C, int e) {
