72dcae974bde8260188d42e064b71256d783c55f
amna <ammy5342@gmail.com>
2025-05-20 00:40:41 +0300

    Implemented Diffie-Hellman

--- Security T3/src/main/java/Security/DiffieHellman.java
+++ Security T3/src/main/java/Security/DiffieHellman.java
@@ -1,9 +1,18 @@
 package Security;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiffieHellman {
     public List<Integer> getKeys(int q, int alpha, int xa, int xb) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        BigInteger bigAlpha = BigInteger.valueOf(alpha);
+        BigInteger bigQ = BigInteger.valueOf(q);
+        BigInteger ya = bigAlpha.pow(xa).mod(bigQ);
+        BigInteger yb = bigAlpha.pow(xb).mod(bigQ);
+        List<Integer> key = new ArrayList<Integer>();
+        key.add(yb.pow(xa).mod(bigQ).intValue());
+        key.add(ya.pow(xb).mod(bigQ).intValue());
+        return key;
     }
 }
