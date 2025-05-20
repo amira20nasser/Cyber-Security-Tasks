e481f22ed282f987d2887b352627dc564f49e284
amna <ammy5342@gmail.com>
2025-05-20 15:32:40 +0300

    Implemnted El Gamal encryption

--- Security T3/src/main/java/Security/ElGamal.java
+++ Security T3/src/main/java/Security/ElGamal.java
@@ -1,13 +1,29 @@
 package Security;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ElGamal {
     public List<Long> encrypt(int q, int alpha, int y, int k, int m) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        BigInteger bigAlpha = BigInteger.valueOf(alpha);
+        BigInteger big_q = BigInteger.valueOf(q);
+        BigInteger big_k = BigInteger.valueOf(k);
+        BigInteger big_y = BigInteger.valueOf(y);
+        BigInteger big_m = BigInteger.valueOf(m);
+        BigInteger c1 = bigAlpha.modPow(big_k, big_q);
+        BigInteger K = big_y.modPow(big_k, big_q);
+
+        BigInteger c2 = K.multiply(big_m).mod(big_q);
+
+        List<Long> ciphertext = new ArrayList<Long>();
+        ciphertext.add(c1.longValue());
+        ciphertext.add(c2.longValue());
+
+        return ciphertext;
     }
 
     public int decrypt(int c1, int c2, int x, int q) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return 0;
     }
 }
